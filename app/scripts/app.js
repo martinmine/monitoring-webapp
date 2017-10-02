aab738a0325cac5d5ae858fe2800b2055a243186
martinmine <martin_mine@hotmail.com>
2017-10-02 17:06:10 +0200

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name monitoringWebappApp
+ * @description
+ * # monitoringWebappApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('monitoringWebappApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name monitoringWebappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the monitoringWebappApp
+ */
+angular.module('monitoringWebappApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
